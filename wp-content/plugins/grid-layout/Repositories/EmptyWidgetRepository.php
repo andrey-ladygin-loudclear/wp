9dc0d958e48331bb8e230347ca15504cacd5cdc8
Srivoknovskiy <Sri>
2017-03-19 20:43:45 +0200

    news widget

---
+++ wp-content/plugins/grid-layout/Repositories/EmptyWidgetRepository.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace GL\Repositories;
+
+use GL\Classes\DB;
+use GL\Interfaces\WidgetRepositoryInterface;
+use JsonSerializable;
+
+Class EmptyWidgetRepository extends DB implements WidgetRepositoryInterface, JsonSerializable {
+	
+	protected static $table;
+	protected $id;
+	
+	public function add() {
+		$this->id = NULL;
+		return $this;
+	}
+	
+	public function remove($id = NULL) {
+		return TRUE;
+	}
+	
+	public function find($widget_id) {
+		$widget_table = self::getTable();
+		$layout_table = LayoutRepository::getTable();
+		
+		$sql = "SELECT * FROM {$layout_table} wgg
+			LEFT JOIN $widget_table wt ON wt.id = wgg.widget_id AND wgg.widget_name = 'glyph'
+			WHERE wt.id = {$widget_id};";
+		
+		return $this->fill($this->query($sql));
+	}
+	
+	public function save($widget_id, $data) {
+		$this->update($data, array('id' => $widget_id));
+	}
+	
+	public function fill(array $attributes) {
+		if(!empty($attributes['widget_id'])) {
+			$this->setId($attributes['widget_id']);
+		}
+		
+		if(!empty($attributes['row'])) {
+			$this->setRow($attributes['row']);
+		}
+		
+		if(!empty($attributes['col'])) {
+			$this->setCol($attributes['col']);
+		}
+		
+		if(!empty($attributes['size_x'])) {
+			$this->setWidth($attributes['size_x']);
+		}
+		
+		if(!empty($attributes['size_y'])) {
+			$this->setHeight($attributes['size_y']);
+		}
+		
+		if(!empty($attributes['full_widget'])) {
+			$this->setFull($attributes['full_widget']);
+		}
+		
+		return $this;
+	}
+	
+	public function getName() {
+		return strtolower(end(explode('\\', static::class)));
+	}
+	
+	public function getPreview() {
+		return '';
+	}
+	
+	public function jsonSerialize() {
+		return array(
+			'widget_id' => $this->getId(),
+			'widget_name' => $this->getName(),
+			'row' => $this->getRow(),
+			'col' => $this->getCol(),
+			'size_x' => $this->getWidth(),
+			'size_y' => $this->getHeight(),
+			//'full_widget' => $this->getFull(),
+			'preview' => $this->getPreview(),
+		);
+	}
+}
--- wp-content/plugins/grid-layout/Widgets/News.php
+++ wp-content/plugins/grid-layout/Widgets/News.php
@@ -9,10 +9,14 @@ use GL\Widgets\System\Widget;
 class News extends Widget {
 
     public function add() {
-        $this->id = 0;
+        $this->id = NULL;
         return $this;
     }
-
+// add NULL value for widget_id in DB
+// add empty widget repository
+// add admin view for widgets
+// add composition of views for admin layouts
+// add ability add list of views for admin layout
 	public function draw() {
 	    View::load("Templates/Frontend/news", array('widget' => $this));
 	}
